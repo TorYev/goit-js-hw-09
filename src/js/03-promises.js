9a21680c970f73f250344c035acb7f46d7cfc368
TorYev <zh.tor17@gmail.com>
2023-11-25 15:40:40 +0100

    task 3 without notiflix

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,34 @@
+import Notiflix from 'notiflix';
+const form = document.querySelector('.form');
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const firstDelay = Number(event.target.elements.delay.value);
+  const delayStep = Number(event.target.elements.step.value);
+  const amount = Number(event.target.elements.amount.value);
+
+  for (let i = 1; i <= amount; i++) {
+    const currentDelay = firstDelay + delayStep * (i - 1);
+    createPromise(i, currentDelay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+});
